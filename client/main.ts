e85fcc442fbce15d279b5abdf2fb955f475ad30d
camila <camila05@mit.edu>
2023-11-30 16:04:36 -0500

    map designing for some areas

--- client/main.ts
+++ client/main.ts
@@ -5,8 +5,7 @@ import { createPinia } from "pinia";
 import piniaPluginPersistedState from "pinia-plugin-persistedstate";
 import { createApp } from "vue";
 
-import VueGoogleMaps from '@fawmi/vue-google-maps'
-
+import VueGoogleMaps from "@fawmi/vue-google-maps";
 
 import App from "./App.vue";
 import router from "./router";
@@ -18,10 +17,10 @@ pinia.use(piniaPluginPersistedState);
 app.use(pinia);
 app.use(router);
 app.use(VueGoogleMaps, {
-    load: {
-        key: 'AIzaSyC5zhtPj89UIAmGzpvjYFEOi8htimHIUiw',
-        libraries: 'places'
-    }
+  load: {
+    key: "AIzaSyC5zhtPj89UIAmGzpvjYFEOi8htimHIUiw",
+    libraries: "places",
+  },
 });
 
 app.mount("#app");
