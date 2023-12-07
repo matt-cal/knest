53761ec6441c5c4162fa54b2a67f03797a4482cd
Matthew Callister <116524474+matt-cal@users.noreply.github.com>
2023-12-07 22:31:25 +0000

    Add back button to create post page

--- client/views/CreatePostView.vue
+++ client/views/CreatePostView.vue
@@ -2,6 +2,7 @@
 import { onBeforeMount, ref } from "vue";
 import { useRoute } from "vue-router";
 import CreatePostForm from "../components/Post/CreatePostForm.vue";
+import router from "../router";
 import { fetchy } from "../utils/fetchy";
 
 const currentRoute = useRoute();
@@ -12,6 +13,10 @@ const parentAreaTitle = ref("");
 const submit = async () => {
   submitted.value = true;
 };
+
+async function back() {
+  void router.push({ name: "Neighborhood", params: { area: areaTitle } });
+}
 
 onBeforeMount(async () => {
   // Typescript complaining about type of currentRoute.params.area
@@ -25,9 +30,12 @@ onBeforeMount(async () => {
 </script>
 <template>
   <div class="main">
+    <div class="back-container">
+      <button class="back" @click="back">Cancel</button>
+    </div>
     <div v-if="!submitted" class="create">
-      <h1>{{ parentAreaTitle }}</h1>
       <h3>New Post</h3>
+      <h1>{{ parentAreaTitle }}</h1>
       <div class="form-container">
         <CreatePostForm @submitted="submit" :areaTitle="areaTitle" />
       </div>
@@ -41,8 +49,25 @@ onBeforeMount(async () => {
 .main {
   height: 100vh;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
   background-color: #caddeb;
+}
+
+.back-container {
+  width: 100%;
+  display: flex;
+}
+
+.back {
+  margin: 8px;
+  width: 75px;
+  height: 28px;
+  background-color: rgb(53, 55, 89);
+  color: white;
+  border-radius: 8px;
+  cursor: pointer;
+  border: none;
 }
 
 .create {
@@ -57,5 +82,6 @@ onBeforeMount(async () => {
 
 h1 {
   font-size: 48px;
+  margin-bottom: 0;
 }
 </style>
