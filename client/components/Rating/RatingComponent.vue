f31cef4ab59bee5f8588d3407a44253f8c77afdc
mhollinger001 <146897530+mhollinger001@users.noreply.github.com>
2023-12-07 15:51:39 -0500

    rating components show correct info
    for the final, i'll stylize it more instead of simple <p> fields

--- client/components/Rating/RatingComponent.vue
+++ client/components/Rating/RatingComponent.vue
@@ -1,11 +1,46 @@
 <script setup lang="ts">
+import { fetchy } from "@/utils/fetchy";
+import { onBeforeMount, ref } from "vue";
 
 const props = defineProps(["areaTitle"]);
+const loaded = ref(false);
 
-const data = {ratings: [{name: 'cost', number: '0.7'}]};
+let data: any[] = [];
+let average: any;
+
+async function getAverage() {
+  try {
+    data = await fetchy(`/api//areas/${props.areaTitle}/reviews`, "GET");
+
+
+    if (data.length > 0) {
+      average = {...data[0].values};
+
+      for (const name in data[0].values) {
+        for (let i = 1; i < data.length; i++) {
+          if (average.hasOwnProperty(name)) {
+            average[name] = average[name] + data[i].values[name];
+          }
+        }
+        average[name] = Math.round(average[name] / data.length);
+      }
+      console.log(average);
+      console.log(data)
+    } 
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+onBeforeMount(async () => {
+  await getAverage();
+  loaded.value = true;
+});
+
 </script>
 <template>
-  <article v-for="rating in data.ratings">
-    <p>average {{ rating.name }}: {{ rating.number }}</p>
+  <p v-if="loaded && data.length == 0">No one has rated this neighborhood... You can be the first!</p>
+  <article v-for="(value, name) in average" :key="name">
+    <p>average {{ name }}: {{ value }} / 100</p>
   </article>
 </template>
